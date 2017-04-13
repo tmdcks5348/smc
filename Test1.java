271be73e7aac1de41b016573481b4c436d514da2
김승찬 <ksc5348@naver.com>
2017-04-13 14:53:40 +0900

    마스터버전

--- Test1.java
+++ Test1.java
@@ -5,13 +5,11 @@ public class Test1 {
 		System.out.println("Hello");
 		System.out.println("yes");
 		System.out.println("world");
-<<<<<<< HEAD
 		System.out.println("world2");
 		System.out.println("world5");
-=======
 		System.out.println("world3");
 		System.out.println("world4");
->>>>>>> sub
+
 
 	}
 }
